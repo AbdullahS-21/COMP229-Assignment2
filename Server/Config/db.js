cf5f21ab71e4970e75ed6d544376700d3fb053f9
AbdullahS-21 <abdullahshaikh225@gmail.com>
2021-06-18 19:49:13 +0530

    db config

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.HostName = exports.LocalURI = void 0;
+exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/contacts";
-exports.HostName = "localhost";
+exports.RemoteURI = "mongodb+srv://admin:A123456@cluster-comp229-a2.sr1ho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,3 +1,4 @@
 export const LocalURI = "mongodb://localhost/contacts";
-export const HostName = "localhost";
+export const RemoteURI = "mongodb+srv://admin:A123456@cluster-comp229-a2.sr1ho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+export const HostName = "RemoteHost";
 export const Secret = "someSecret";
